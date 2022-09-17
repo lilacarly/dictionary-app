963b823934ea12080c1842c1f2a98ef6dedf6761
lilacarly <108032321+lilacarly@users.noreply.github.com>
2022-09-17 14:12:37 -0400

    some initial stylings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -35,8 +35,13 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
+          <h1>Search for a word:</h1>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} />
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
         </section>
         <Results results={results} />
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,13 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
+      <span className="text">{props.phonetic.text}</span>
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
-      <br />
-      {props.phonetic.text}
     </div>
   );
 }
