33fbb25c9df39fbe41b674ee3cf54996889e398f
lilacarly <108032321+lilacarly@users.noreply.github.com>
2022-09-17 11:03:00 -0400

    added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
+import axios from "axios";
+
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse() {}
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
