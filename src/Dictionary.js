2d5333e3620775492e0467c8dded99b75c763b86
lilacarly <108032321+lilacarly@users.noreply.github.com>
2022-09-17 12:05:31 -0400

    search and display
    The search is working, and displays definitions, parts of speech, examples.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
-  function handleResponse() {}
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
   function search(event) {
     event.preventDefault();
@@ -22,6 +26,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
